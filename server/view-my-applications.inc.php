7ad8675968f8c9f898e2e5d25225671ab50b040a
Clementine Mamogale <clementinemamogale@gmail.com>
2021-12-04 14:31:46 +0200

    Fixed dynamic files on view applications

--- server/view-my-applications.inc.php
+++ server/view-my-applications.inc.php
@@ -1,54 +1,148 @@
 <?php
-    //session_start();
-    if($_SERVER['REQUEST_METHOD'] != "POST"){
+    session_start();
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
         if(isset($_SESSION['s_id']) && preg_match('/^[a-zA-Z0-9]+$/', $_SESSION['s_id'])){
             $user = $_SESSION['s_id'];
-            
+            ?>
+                <h4 style="text-align: center; margin-bottom: 3%">View your accommodation applications</h4>
+                <p class="err" id="err_msg"></p>
+            <?php
+                
             $sql = "SELECT new_applicants.accommodation, new_applicants.action_date, new_applicants.a_status 
-                    FROM (new_applicants 
-                        INNER JOIN applications ON new_applicants.id = applications.app_id)
-                    WHERE applications.id = \"$user\" 
-                        AND (new_applicants.a_status = \"1\" OR  new_applicants.a_status = \"2\" OR  new_applicants.a_status = \"3\")  
+                    FROM (application 
+                        INNER JOIN new_applicants ON application.app_id = new_applicants.id)
+                    WHERE application.id = \"$user\" 
+                        AND (new_applicants.a_status = \"0\" OR  new_applicants.a_status = \"1\" OR new_applicants.a_status = \"1\" OR  
+                            new_applicants.a_status = \"2\" OR  new_applicants.a_status = \"3\")  
                     LIMIT 20";
+            //echo $sql;
             require("../includes/conn.inc.php");
             $sql_results = new SQL_results();
             $results = $sql_results->results_applicaations($sql);
             $applications = array();
+            $select_options = $message = "";
             if ($results->num_rows > 0) {
                 while($row = $results->fetch_assoc()){
-                    $temp_applications = array("accommodation"=>$row['accommodation'],
-                                          "address"=>"",
-                                          "status"=>$row['a_status'],
-                                          "date"=>$row['action_date']);
+                    $temp_applications = array("id"=>$row['accommodation'],
+                                               "name"=>"",
+                                               "address"=>"",
+                                               "status"=>$row['a_status'],
+                                               "date"=>date("d M Y", strtotime(substr($row['action_date'], 0, 10))));
                     array_push($applications, $temp_applications);
                 }
-                
                 if(sizeof($applications) > 0){
-                    foreach($applications as $application => $value){
-                        $accommodation = $applications[$application]['accommodation'];
-                        $sql = "SELECT accommodations.name, address.main_address
-                                FROM ( accommodations
-                                    INNER JOIN address ON accommodations.id = address..accommo_id)
-                                WHERE accommodations.id = \"$accommodation\" LIMIT 1";
-                        $results = $sql_results->results_applicaations($sql);
-                        if ($results->num_rows > 0) {
-                            $row = $results->fetch_assoc();
-                            $applications[$application]['accommodation'] = $row['name'];
-                            $applications[$application]['address'] = $row['address'];
-                        }
-                        if($applications[$application]['address'] != ""){
-                            $temp_address = explode(",", str_replace("<br>", ",", $applications[$application]['address']));
-                            for($i = 0; $i < 4; $i++){
-                                if($temp_address[$i] != "") $a_address .= $temp_address[$i] . ", ";
-                                else continue;
+                    ?>
+                    <small style='color: gray'><i>Please view in your emails for details about your accommodation application statuses</i></small>
+                    <table>
+                        <colgroup>
+                            <col span="1">
+                            <col span="1">
+                            <col span="1">
+                            <col span="1">
+                            <col span="1">
+                        </colgroup>
+                        <tbody>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Address</th>
+                                <th>Status</th>
+                                <th>Date <br>approved/<br>rejected</th>
+                            </tr>
+                            <?php
+                            $counter = 1;
+                            foreach($applications as $application => $value){
+                                $accommodation = $applications[$application]['id'];
+                                $sql = "SELECT accommodations.name, address.main_address
+                                        FROM ( accommodations
+                                            INNER JOIN address ON accommodations.id = address.accommo_id)
+                                        WHERE accommodations.id = \"$accommodation\" LIMIT 1";
+                                //echo $sql;
+                                $results = $sql_results->results_accommodations($sql);
+                                if ($results->num_rows > 0) {
+                                    $row = $results->fetch_assoc();
+                                    $applications[$application]['name'] = $row['name'];
+                                    $applications[$application]['address'] = $row['main_address'];
+                                }
+                                if($applications[$application]['address'] != ""){
+                                    $temp_address = explode(",", str_replace("<br>", ",", $applications[$application]['address']));
+                                    $a_address = "";
+                                    for($i = 0; $i < 4; $i++){
+                                        if($temp_address[$i] != "") $a_address .= $temp_address[$i] . ", ";
+                                        else continue;
+                                    }
+                                    $applications[$application]['address'] = substr($a_address, 0, (strlen($a_address) - 2));
+                                }
+                                ?>
+                                <tr>
+                                    <td><?php echo $counter; ?></td>
+                                    <td><?php echo $applications[$application]['name']; ?></td>
+                                    <td><?php echo $applications[$application]['address']; ?></td>
+                                    <td>
+                                        <?php
+                                            if($applications[$application]['status'] == 0){
+                                                echo '<span style="color: orange">Peding</span>';
+                                            }else if($applications[$application]['status'] == 1){
+                                                echo '<span style="color: green"><i>You</i> Accepted offer</span>';
+                                            }else if($applications[$application]['status'] == 2){
+                                                echo '<span style="color: red">Rejected</span>';
+                                            }else if($applications[$application]['status'] == 3){
+                                                echo '<span style="color: blue">Pending <i>Your</i> approval/rejection</span>';
+                                                $select_options .= '<option value="' . $applications[$application]['id'] . '">' .   
+                                                                            $applications[$application]['name'] . '</option>';
+                                                if($message == ""){
+                                                    $message = '<span style="color: blue">Congradulations, one of your applicatoins have been approved. Please select to approve or reject the offer</span>';
+                                                }
+                                            }else if($applications[$application]['status'] == 4){
+                                                echo '<span style="color: red"><i>You</i> Rejected offer</span>';
+                                            }
+                                        ?>
+                                    </td>
+                                    <td><?php echo $applications[$application]['date']; ?></td>
+                                    <!-- 22 Apr 2021 -->
+                                    <td>
+                                        <span class="fas fa-trash" style='color: red' 
+                                                onclick="delete_application('<?php echo $applications[$application]['id']; ?>')">
+                                        </span>
+                                    </td>
+                                </tr>
+                                <?php
+                                $counter++;
                             }
-                            $applications[$application]['address'] = substr($a_address, 0, (strlen($a_address) - 2));
-                        }
-                    }   
+                        ?>
+                        </tbody>
+                    </table>
+                    <span id="temp_message" style="display: none"><?php echo $message; ?></span>
+                    <?php 
+                    
+                    if($select_options != ""){
+                        ?>
+                        <div id="actions">
+                            <p>Select the accommodation you got approved and action to take</p>
+                            <small style="color: red">
+                                Please note that once you accept/decline an offer, you cannot change that until it's been reviewed by the maanager.
+                            </small><br>
+                            <span id="err_submit_msg"></span>
+                            <select id="accommodation_name">
+                                <?php echo $select_options; ?>
+                            </select>
+                            <select id="action">
+                                <option value="approve">Approve</option>
+                                <option value="reject">Reject</option>
+                            </select>
+                            <button id="submit_results">Submit</button>
+                        </div>
+                        <script type="text/javascript">
+                            $(document).ready(function(){
+                                $("#err_msg").html($("#temp_message").html());  
+                            });
+                        </script>
+                        <?php
+                    }
                 }
             }else{
-                echo "<h5>No applications found<h5>
-                        <p>You can click <a href='#'>here</a> to browse for accommodations to apply for one</p>";
+                $message = "<h5>No applications found<h5>
+                        <p>You can click <a href='./featured.php'>here</a> to browse for accommodations to apply for one</p>";
                 return;
             }
         }else{
--- view-my-applications.php
+++ view-my-applications.php
@@ -13,88 +13,7 @@
             }
         ?>  
         <link rel="stylesheet" type="text/css" href="./css/style-view-my-accommodation-applications.css">
-        <div id="table_results">
-            <h4 style="text-align: center; margin-bottom: 3%">View your accommodation applications</h4>
-            <p class="err">
-                <span style="color: blue">
-                    Congradulations, one of your applicatoins have been approved. Please select to approve or reject the offer
-                </span>
-            </p>
-            <small style='color: gray'><i>Please view in your emails for details about your accommodation application statuses</i></small>
-            <table>
-                <colgroup>
-                    <col span="1">
-                    <col span="1">
-                    <col span="1">
-                    <col span="1">
-                    <col span="1">
-                </colgroup>
-                <tbody>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Address</th>
-                        <th>Status</th>
-                        <th>Date <br>approved/<br>rejected</th>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>West Liffe Residence</td>
-                        <td>101 Smith Street, Hillbrow, Johannesburg, 2001</td>
-                        <td><span style="color: orange">Peding</span></td>
-                        <td>22 Apr 2021</td>
-                        <td><span class="fas fa-trash" style='color: red' onclick="delete_application('123')"></span></td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Momo's Place</td>
-                        <td>22 End Street, Doornfontein, Johannesburg, 2001</td>
-                        <td><span style="color: red">Rejected</span></td>
-                        <td>16 Jun 2020</td>
-                        <td><span class="fas fa-trash" style='color: red' onclick="delete_application('123')"></span></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>West Liffe Residence</td>
-                        <td>16 Thabo's Street, Pretoria, 5547</td>
-                        <td><span style="color: blue">Pending <i>Your</i> approval/rejection</span></td>
-                        <td>02 May 2021</td>
-                        <td><span class="fas fa-trash" style='color: red' onclick="delete_application('123')"></span></td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>West Liffe Residence</td>
-                        <td>101 Smith Street, Hillbrow, Johannesburg, 2001</td>
-                        <td><span style="color: green"><i>You</i> Accepted offer</span></td>
-                        <td>16 Dec 2021</td>
-                        <td><span class="fas fa-trash" style='color: red' onclick="delete_application('123')"></span></td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>West Liffe Residence</td>
-                        <td>101 Smith Street, Hillbrow, Johannesburg, 2001</td>
-                        <td><span style="color: red"><i>You</i> Rejected offer</span></td>
-                        <td>16 Dec 2021</td>
-                        <td><span class="fas fa-trash" style='color: red' onclick="delete_application('123')"></span></td>
-                    </tr>
-                </tbody>
-            </table>
-            <div id="actions">
-                <p>Select the accommodation you got approved and action to take</p>
-                <small style="color: red">
-                    Please note that once you accept/decline an offer, you cannot change that until it's been reviewed by the maanager.
-                </small><br>
-                <select id="accommodation_name">
-                    <option value="15Ksh2544S">West life residence</option>
-                    <option value="15224WsfDfLS">Momo's Place</option>
-                </select>
-                <select id="action">
-                    <option value="approve">Approve</option>
-                    <option value="reject">Reject</option>
-                </select>
-                <button id="submit_results">Submit</button>
-            </div>
-        </div>    
+        <div id="table_results"></div>    
     </div>
     <div class="col-sm-1"></div>
     </div>
@@ -109,6 +28,10 @@
 	<script src="js/validate_email.js" type="text/javascript"></script>
 	<script src="js/footer.js" type="text/javascript"></script>
     <script type="text/javascript">
+        $(document).ready(function(){
+            let url = "./server/view-my-applications.inc.php";
+            send_data(url, displayer, "#table_results")
+        });
         function delete_application(payload){
             let con = confirm("Are you sure you want to delete this applications? You cannot recover it once deleted");
             if(con == true){
@@ -118,6 +41,18 @@
         }
         function delete_application_helper(data, loc){
             alert(data);
+        }
+        function send_response(){
+            let accommodation = $("#accommodation_name").val();
+            let action = $("#action").val();
+            if(accommodation == "" || action == ""){
+                $("err_submit_msg").html("Please select both accommodation name and action to take before submit<br>");
+            }else{
+                $("err_submit_msg").html("");
+                return;
+            }
+            let url = "./server/delete-application.php?accommodation=" + accommodation + "&action=" + action;
+            send_data(url, delete_application_helper, "");
         }
     </script>
 </body>      
