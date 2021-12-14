3e6fb3732bbbb8e2aff7b154e2a9df3c83356eda
Clementine Mamogale <clementinemamogale@gmail.com>
2021-12-14 17:29:40 +0200

    Added more pages and fixed bugs

--- management-accommodations/server/accepted-results.php
+++ management-accommodations/server/accepted-results.php
@@ -35,13 +35,13 @@
             echo "Internal errror occured. Please reload page and try again.";
 			return;
 		}
-
 		require_once '../../includes/conn.inc.php';
 		$db_login = new DB_login_updates();
 		$connection = $db_login->connect_db("applications");
 		$new_load = explode(",", $load);
 		foreach ($new_load as $new_data => $value) {
 			$temp_id = $value;
+            $notif = true;
 			$sql_update = "UPDATE new_applicants 
 						   SET a_status = \"3\"
 						   WHERE id = \"$temp_id\"";
@@ -51,45 +51,48 @@
 				$a_id = $temp_id;
 				$sql = "SELECT full_names, surname, email, accommodation 
 						FROM new_applicants
-						WHERE id = \"$temp_id\" AND a_status = 1
+						WHERE id = \"$temp_id\" AND a_status = 3 AND notif = \"0\"
 						LIMIT 1";
+						echo $sql;
 				$sql_results = new SQL_results();
 				$results = $sql_results->results_applicaations($sql);
 				if($results->num_rows > 0){
 					$row = $results->fetch_assoc();
+        			$sql_update = "UPDATE new_applicants 
+        						   SET  notif = \"1\"
+        						   WHERE id = \"$temp_id\"";
+        			if ($connection->query($sql_update) == true){
+        			    //do nothing 
+        			}
+
                     if(true){
                         $full_name = strtoupper(substr($row['surname'], 0, 1)) . substr($row['surname'], 1); 
                         $full_name .= " " . strtoupper(substr($row['full_names'], 0, 1)) . substr($row['full_names'], 1); 
                         $email = $row['email'];
                         $temp_id = $row['accommodation'];
 					}
-				}
-				$sql = "SELECT name 
-						FROM accommodations
-						WHERE id = \"$temp_id\"
-						LIMIT 1";
-				$sql_results = new SQL_results();
-				$results = $sql_results->results_accommodations($sql);
-				if($results->num_rows > 0){
-					$row = $results->fetch_assoc();
-                    if(true){
-                        $a_name = strtoupper(substr($row['name'], 0, 1)) . substr($row['name'], 1); 
-					}
-				}
+				}else{
+				    $notif = false;
+                } 
+				if($notif == true){
+                    $sql = "SELECT name 
+                            FROM accommodations
+                            WHERE id = \"$temp_id\"
+                            LIMIT 1";
+                    $sql_results = new SQL_results();
+                    $results = $sql_results->results_accommodations($sql);
+                    if($results->num_rows > 0){
+                        $row = $results->fetch_assoc();
+                        if(true){
+                            $a_name = strtoupper(substr($row['name'], 0, 1)) . substr($row['name'], 1); 
+                        }
+                    }
 
-				/*
-				Set the following before using this file
-				$name -> Name from where the email is from
-				$subject -> subject of the email
-				$email -> your email address, or where the eamil is from 
-				$recipient -> the address where the message is sent to
-				$message-> the message 
-				*/
-				$name = "Obocircle";
-				$subject = "Accommodation applications status";
-				$recipient = $email;
-				$email = "no-reply@obocircle.com";
-				$message = "
+                    $name = "Obocircle";
+                    $subject = "Accommodation applications status";
+                    $recipient = $email;
+                    $email = "no-reply@obocircle.com";
+                    $message = "
 Dear " . $full_name . ", 
 
 This is to let you know that your applications to apply for accommodation at " . $a_name . " has been accepted by the manager. 
@@ -101,7 +104,7 @@ NB. This is a formal confirmation of you application being accepted from Obocirc
 Thank you for choosing us. Please can you take few minutes of your time to participate in this survey bolow.
 https://obocircle.com/survey.php?accommodation=" . $a_id . "
 This survey is anonymous, it is used to measure the level of service that we provide to our clients and to let us know where and how to improve for better services.  
-				
+
 Warm regards, 
 
 Obocircle.com
@@ -109,8 +112,9 @@ https://obocircle.com/
 
 Obocircle.com copyright © " . date('Y') . " | all rights reserved
 Powered and maintained by https://www.mcnetsolutions.co.za [McNet Solutions (Pty) Ltd]";
-				//require '../../server/send-email.php';
-			}else echo "Error: " . $connection->error;
+                    //require '../../server/send-email.php';
+                }   
+            }
 		}
 		$connection->close();
 	}else{
